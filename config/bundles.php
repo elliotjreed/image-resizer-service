1782c3559402bb59e72f87e08373eeef29f30fa9
Elliot J. Reed <git@elliotjreed.com>
2022-04-11 19:23:27 +0100

    Adds strict types

--- config/bundles.php
+++ config/bundles.php
@@ -1,8 +1,10 @@
 <?php
 
+declare(strict_types=1);
+
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
-    Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
+    Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true]
 ];
